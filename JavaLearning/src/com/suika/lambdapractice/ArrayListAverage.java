eaa6f88210c4505654a98c74553f0a082775520f
suika <suika@stevens-air.lan>
2021-01-19 18:27:52 -0800

    This fixes the ArrayList Average Class by making the method static so no instance of the class has to be made

--- JavaLearning/src/com/suika/lambdapractice/ArrayListAverage.java
+++ JavaLearning/src/com/suika/lambdapractice/ArrayListAverage.java
@@ -4,13 +4,9 @@ import java.util.ArrayList;
 
 public class ArrayListAverage 
 {
-	ArrayList<Double> numList;
 	
-	public ArrayListAverage(ArrayList<Double> numList) {
-		this.numList = numList;
-	}
 	
-	public double getAverage(){
+	public static double getAverage(ArrayList<Double> numList){
 		return (numList.stream().reduce(0.0, (num1, num2) -> num1 + num2)) 
 				/ (numList.stream().count());
 	}
--- JavaLearning/src/com/suika/main/Main.java
+++ JavaLearning/src/com/suika/main/Main.java
@@ -1,5 +1,7 @@
 package com.suika.main;
+import java.util.ArrayList;
 
+import com.suika.lambdapractice.*;
 public class Main {
 
 	public static void main(String[] args) {
@@ -7,7 +9,13 @@ public class Main {
 		Integer num1 = 42;
 		Integer num2 = 35;
 		System.out.println(num1 + num2);
-		//ArrayList<Integer>
+		ArrayList<Double> numList = new ArrayList<>();
+		numList.add(3.1);
+		numList.add(3.5);
+		numList.add(4.6);
+		numList.add(6.8);
+		numList.add(2.2);
+		System.out.println(ArrayListAverage.getAverage(numList));
 	}
 
 }
